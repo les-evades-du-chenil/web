97c36e16b88203c20a0ccf112038b9c19b91def4
Swann <swann@devloup.dev>
2021-03-17 00:42:12 +0100

    add tailwind + login-page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  // IN PRODUCTION ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
