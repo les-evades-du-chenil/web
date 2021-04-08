55c18453ddd73414f736667a5e66290dab38107e
Swann <swann@devloup.dev>
2021-04-08 23:35:03 +0200

    add the refresh_token logique

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import { ApolloProvider } from "@apollo/client";
 import { apolloClient } from "../wsProvider";
 import { AppProps } from "next/app";
-import React from "react";
+import React, { useEffect } from "react";
 import "tailwindcss/tailwind.css";
+import { setAccessToken } from "../package/auth/accessToken";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    fetch("http://localhost:8080/user/refresh_token", {
+      method: "POST",
+      credentials: "include",
+    }).then(async (res) => {
+      const { accessToken } = await res.json();
+      setAccessToken(accessToken);
+    });
+  }, []);
+
   return (
     <ApolloProvider client={apolloClient}>
       <Component {...pageProps} />
